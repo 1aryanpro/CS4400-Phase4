633df709e2c2c916508d5a262f74617a49723ece
sdas64 <sandas.edu@gmail.com>
2025-04-27 19:56:52 -0400

    Update +page.svelte
    Made View Tables, need to do styling

--- src/routes/views/[viewName]/+page.svelte
+++ src/routes/views/[viewName]/+page.svelte
@@ -5,16 +5,34 @@
 
     let viewName = data.viewName;
     let viewData = data.viewData;
+
+    let tableHTML = '';
+
+    if (viewData && viewData.length > 0) {
+        const keys = Object.keys(viewData[0]);
+
+        tableHTML += '<table>';
+        
+        tableHTML += '<thead><tr>';
+        for (const key of keys) {
+            tableHTML += `<th>${snakeToTitleCase(key)}</th>`;
+        }
+        tableHTML += '</tr></thead>';
+        
+        tableHTML += '<tbody>';
+        for (let i = 0; i < viewData.length; i++) {
+            tableHTML += '<tr>';
+            for (const key of keys) {
+                tableHTML += `<td>${viewData[i][key]}</td>`;
+            }
+            tableHTML += '</tr>';
+        }
+        tableHTML += '</tbody>';
+
+        tableHTML += '</table>';
+    }
 </script>
 
 <h1>View: {snakeToTitleCase(viewName)}</h1>
 
-{#if viewData && viewData.length > 0}
-    <ul>
-        {#each viewData as row}
-            <li>{JSON.stringify(row)}</li>
-        {/each}
-    </ul>
-{:else}
-    <p>No data found.</p>
-{/if}
+{@html tableHTML}
